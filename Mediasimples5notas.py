190e1e97a06da0272998d9cc09b210572eda3d4b
Lucas Moro <lucasmoroo771@gmail.com>
2022-10-05 15:01:40 -0300

    Add files via upload
    Programa simples de média de 5 notas de um aluno.

---
+++ Mediasimples5notas.py
@@ -0,0 +1,15 @@
+print("==== Programa média do aluno ====")
+
+
+nota1=float(input("Digite a primeira nota do aluno.\n"))
+nota2=float(input("Digite a segunda nota do aluno.\n"))
+nota3=float(input("Digite a terceira nota do aluno.\n"))
+nota4=float(input("Digite a quarta nota do aluno.\n"))
+nota5=float(input("Digite a quinta nota do aluno.\n"))
+
+
+media=(nota1+nota2+nota3+nota4+nota5/5)
+
+print(f"A media do aluno é de {media:6.2f}")
+
+
